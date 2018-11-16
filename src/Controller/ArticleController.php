d660b74759d7fb4c145c26242fdd31d162cddc38
Beskargam <horg_hvergelmir@hotmail.com>
2018-11-16 17:52:46 +0100

    set routes tree for app

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Aure
- * Date: 15.11.2018
- * Time: 16:42
- */
 
 namespace App\Controller;
 
@@ -15,11 +9,38 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
 
     public function homepage()
     {
         return $this->render('home/home.html.twig');
+    }
+
+    /**
+     * @Route("/figure/{id}", name="app_trick")
+     */
+
+    public function view($id)
+    {
+        return $this->render('trick/trick.html.twig');
+    }
+
+    /**
+     * @Route("/ajouter", name="app_add")
+     */
+
+    public function add()
+    {
+        return $this->render('trick/add.html.twig');
+    }
+
+    /**
+     * @Route("/editer/{id}", name="app_edit")
+     */
+
+    public function edit($id)
+    {
+        return $this->render('trick/edit.html.twig');
     }
 }
