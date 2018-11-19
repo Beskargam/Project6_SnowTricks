15841ded332d57d22196557815e9e157e3368cc2
Beskargam <horg_hvergelmir@hotmail.com>
2018-11-19 09:58:44 +0100

    route improvements with prefix

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -6,39 +6,38 @@ namespace App\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/**
+ * @Route("/", name="app_")
+ */
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/", name="app_homepage")
+     * @Route("/", name="homepage")
      */
-
     public function homepage()
     {
         return $this->render('home/home.html.twig');
     }
 
     /**
-     * @Route("/figure/{id}", name="app_trick")
+     * @Route("/figure/{id}", name="trick")
      */
-
     public function view($id)
     {
         return $this->render('trick/trick.html.twig');
     }
 
     /**
-     * @Route("/ajouter", name="app_add")
+     * @Route("/ajouter", name="add")
      */
-
     public function add()
     {
         return $this->render('trick/add.html.twig');
     }
 
     /**
-     * @Route("/editer/{id}", name="app_edit")
+     * @Route("/editer/{id}", name="edit")
      */
-
     public function edit($id)
     {
         return $this->render('trick/edit.html.twig');
